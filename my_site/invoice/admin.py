74e6bde5a36d138ca4d2f45ae8729e88a2b0fe3a
Akinropo Tobi Israel <tobiropo@gmail.com>
2024-01-18 17:52:21 +0100

    add invoice_date to invoice schema and make migrations

--- my_site/invoice/admin.py
+++ my_site/invoice/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import BusinessProfile, AccountDetail, CustomerProfile, Invoice, InvoiceItem, InvoiceTotal
 
 # Register your models here.
+admin.site.register(BusinessProfile)
+admin.site.register(AccountDetail)
+admin.site.register(CustomerProfile)
+admin.site.register(Invoice)
+admin.site.register(InvoiceItem)
+admin.site.register(InvoiceTotal)
