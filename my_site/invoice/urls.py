40deafef37b86104b919a6a0742ba62339e8157f
Akinropo Tobi Israel <tobiropo@gmail.com>
2024-01-23 22:38:31 +0100

    added design touchup and view, edit and delete functionalities

--- my_site/invoice/urls.py
+++ my_site/invoice/urls.py
@@ -4,11 +4,14 @@ from . import views
 app_name = "invoice"
 
 urlpatterns = [
-    path('',views.HomeListView.as_view(),name='home'),
+    path('',views.HomeView.as_view(),name='home'),
+    path('business_list/',views.BusinessListView.as_view(),name='business'),
     path('invoice_list/',views.InvoiceListView.as_view(), name='invoice_list'),
     path('create_business/',views.AddBusinessView.as_view(), name='add_business'),
     path('create_customers/', views.CustomerCreateView.as_view(), name='create_customer'),
     path('create_invoice/',views.InvoiceCreateView.as_view(), name='create_invoice'),
     path('invoice_detail/<slug:slug>', views.invoicedetail,name='invoice_detail'),
-    path('customer_detail/<int:id>', views.customerdetail,name='customer_detail')
+    path('delete_invoice/<slug:slug>/', views.invoicedelete,name='delete_invoice'),
+    path('delete_customer/<int:id>', views.deletecustomer,name='delete_customer'),
+    path('delete_business/<int:id>/', views.deletebusiness, name='delete_business')
 ]
