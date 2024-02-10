2e393b6d9c4fa33d75e441b929305c86f44c3382
Akinropo Tobi Israel <isaacakinropo@gmail.com>
2024-02-10 07:44:17 +0100

    remove debug from settings.py

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z_48f1pov3k_hp(r2m%eyod&z2j)v9yd-y9*gdpkr@ga!-c^jd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
